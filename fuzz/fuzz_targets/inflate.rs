b7618c488a1303bf31dbfb0823435e0721643507
Jonathan Behrens <fintelia@gmail.com>
2022-12-12 19:53:17 -0800

    Add a corpus for fuzzing inflate

--- fuzz/fuzz_targets/inflate.rs
+++ fuzz/fuzz_targets/inflate.rs
@@ -2,26 +2,23 @@
 use libfuzzer_sys::fuzz_target;
 use std::io::{Cursor, Read};
 
-fuzz_target!(|input: (&[u8], bool)| {
-    let mut decompressed = vec![0; input.0.len() * 2 + 2];
+fuzz_target!(|input: &[u8]| {
+    let mut decompressed = vec![0; input.len() * 2 + 2];
     let mut decompressor = fdeflate::Decompressor::new();
 
-    match decompressor.read(&input.0, &mut decompressed, input.1) {
+    match decompressor.read(&input, &mut decompressed, true) {
         Ok(n) => {
-            if decompressor.done() {
-                let mut decompressed2 = Vec::new();
-                flate2::bufread::ZlibDecoder::new(Cursor::new(&input.0))
-                    .read_to_end(&mut decompressed2)
-                    .unwrap();
-                assert_eq!(decompressed[..n.1], decompressed2);
-            } else {
-                assert!(!input.1);
-            }
+            assert!(decompressor.done());
+            let mut decompressed2 = Vec::new();
+            flate2::bufread::ZlibDecoder::new(Cursor::new(&input))
+                .read_to_end(&mut decompressed2)
+                .unwrap();
+            assert_eq!(decompressed[..n.1], decompressed2);
         }
         Err(fdeflate::DecompressionError::CorruptData) => {
             match flate2::Decompress::new(true).decompress_vec(
-                &input.0,
-                &mut Vec::with_capacity(input.0.len() * 2 + 2),
+                &input,
+                &mut Vec::with_capacity(input.len() * 2 + 2),
                 flate2::FlushDecompress::Finish,
             ) {
                 Ok(flate2::Status::BufError) | Err(_) => {}
