c6088004597b38b4e9aac5a5e60e2465357efceb
Jonathan Behrens <fintelia@gmail.com>
2023-03-13 23:33:54 -0700

    Update inflate fuzz test

--- fuzz/fuzz_targets/inflate.rs
+++ fuzz/fuzz_targets/inflate.rs
@@ -1,7 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use miniz_oxide::inflate::TINFLStatus;
-use std::io::Read;
 
 fuzz_target!(|input: &[u8]| {
     if input.is_empty() {
@@ -14,22 +13,16 @@ fuzz_target!(|input: &[u8]| {
                 return;
             }
             let Ok(decompressed2) = miniz_oxide::inflate::decompress_to_vec_zlib(&input) else {return};
-            // let min_len = decompressed.len().min(decompressed2.len());
-            // assert_eq!(decompressed[..min_len], decompressed2[..min_len]);
-            // assert_eq!(decompressed.len(), decompressed2.len());
             assert_eq!(decompressed, decompressed2);
         }
         Err(fdeflate::DecompressionError::BadLiteralLengthHuffmanTree) => {}
+        Err(fdeflate::DecompressionError::InvalidDistanceCode) => {}
         Err(err) => match miniz_oxide::inflate::decompress_to_vec_zlib(&input) {
             Err(r)
                 if r.status == TINFLStatus::Failed
                     || r.status == TINFLStatus::FailedCannotMakeProgress => {}
             r => {
-                if let Err(e) = flate2::read::ZlibDecoder::new(std::io::Cursor::new(input))
-                    .read_to_end(&mut Vec::new())
-                {
-                    panic!("fdeflate: {:?}, miniz_oxide: {:?}, zlib: {:?}", err, r, e);
-                }
+                panic!("fdeflate: {:?}, miniz_oxide: {:?}", err, r);
             }
         },
     }
